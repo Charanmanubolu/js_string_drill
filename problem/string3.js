b8a0d5fdb8f31f97466bb20be4dcf26f37a36d01
charnamanubolu <charanmanubolu@gmail.com>
2024-02-27 11:45:20 +0530

    problem solved

--- problem/string3.js
+++ problem/string3.js
@@ -3,3 +3,26 @@
 // ==== String Problem #3 ====
 // Given a string in the format of "20/1/2021", print the month in which the date is present in.
 
+function problem3(string){
+    let date = new Date(string)
+    let month = date.getMonth();
+    let monthName= [
+        "January", 
+        "February", 
+        "March", 
+        "April", 
+        "May", 
+        "June", 
+        "July", 
+        "August", 
+        "September", 
+        "October", 
+        "November", 
+        "December"
+      ];
+      return monthName[month];
+
+      
+}
+
+module.exports= problem3;
